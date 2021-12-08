745e6b4072ac830af4cd5b6e802b62cde5c9cede
Mikheull <Mikhael.bailly1@orange.fr>
2021-12-08 16:55:39 +0100

    Fix enum option / Add Get reactions method

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    Model = mongoose.Model,
-    ObjectId = Schema.Types.ObjectId;
+    Model = mongoose.Model;
 
 module.exports = exports = function(schema, options) {
 	options = options || {};
   // console.log(options);
 
-  const reactionType = (options.set) ? {type: String, enum: options.set} : {type: String} ;
+  const reactionType = (options.set) ? {type: String, enum: options.set} : {type: String};
+  const userModel = (options.userModel) ? options.userModel : 'User';
 
 	schema.add({
 		reactions: [
 			{
 				reaction: reactionType,
-				reactBy: ObjectId
+				reactBy: {type: Schema.Types.ObjectId, ref: userModel}
 			}
 		]
 	});
 
+  /**
+   *
+   * @returns Array
+   */
+  schema.methods.getAvailableReactions = function () {
+    return (options.set) ? options.set : {message: 'No restrictions with the reactions'};
+	};
+
+
+  /**
+   *
+   * @returns Object
+   */
+  schema.methods.getReactions = function () {
+    const groupByKey = (list, key) => list.reduce((hash, obj) => ({...hash, [obj[key]]:( hash[obj[key]] || [] ).concat(obj)}), {})
+    return groupByKey(this.reactions, 'reaction');
+	};
+
+
+  /**
+   *
+   * @param {*} reactionType
+   * @param {*} userId
+   * @returns Object
+   */
 	schema.methods.addReaction = function (reactionType, userId) {
-		let newReaction = {
+    if(options.set && !options.set.includes(reactionType)){
+      throw new Error('This reaction is not allowed on this document');
+    }
+
+		const newReaction = {
 			reactBy: userId,
 			reaction: reactionType
 		};
 
-    // TODO : Verifier si le enum est bon
-    const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString());
+    const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString() && x.reaction === reactionType);
     if(!FOUND){
       this.reactions = this.reactions.concat([newReaction]);
       return this.save();
@@ -34,6 +62,13 @@ module.exports = exports = function(schema, options) {
     return this;
 	};
 
+
+  /**
+   *
+   * @param {*} reactionType
+   * @param {*} userId
+   * @returns Object
+   */
 	schema.methods.removeReaction = function (reactionType, userId) {
 		for (let i = 0; i < this.reactions.length; i++) {
 			let r = this.reactions[i];
@@ -47,7 +82,18 @@ module.exports = exports = function(schema, options) {
 		return this;
 	};
 
+
+  /**
+   *
+   * @param {*} reactionType
+   * @param {*} userId
+   * @returns Object
+   */
 	schema.methods.toggleReaction = function (reactionType, userId) {
+    if(options.set && !options.set.includes(reactionType)){
+      throw new Error('This reaction is not allowed on this document');
+    }
+
 		for (let i = 0; i < this.reactions.length; i++) {
 			let r = this.reactions[i];
 
@@ -57,13 +103,12 @@ module.exports = exports = function(schema, options) {
 			}
 		}
 
-		let newReaction = {
+		const newReaction = {
 			reactBy: userId,
 			reaction: reactionType
 		};
 
-    // TODO : Verifier si le enum est bon
-    const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString());
+    const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString() && x.reaction === reactionType);
     if(!FOUND){
       this.reactions = this.reactions.concat([newReaction]);
       return this.save();
