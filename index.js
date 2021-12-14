cd6f78340686feca41b819495132366a91bacd58
Mikheull <Mikhael.bailly1@orange.fr>
2021-12-14 23:56:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    Model = mongoose.Model;
+	Schema = mongoose.Schema,
+	Model = mongoose.Model;
 
 module.exports = exports = function(schema, options) {
 	options = options || {};
-  // console.log(options);
+	// console.log(options);
 
-  const reactionType = (options.set) ? {type: String, enum: options.set} : {type: String};
-  const userModel = (options.userModel) ? options.userModel : 'User';
+	const reactionType = (options.set) ? {type: String, enum: options.set} : {type: String};
+	const userModel = (options.userModel) ? options.userModel : 'User';
 
 	schema.add({
 		reactions: [
@@ -33,7 +33,7 @@ module.exports = exports = function(schema, options) {
    */
   schema.methods.getReactions = function () {
     const groupByKey = (list, key) => list.reduce((hash, obj) => ({...hash, [obj[key]]:( hash[obj[key]] || [] ).concat(obj)}), {})
-    return groupByKey(this.reactions, 'reaction');
+		return groupByKey(this.reactions, 'reaction');
 	};
 
 
@@ -44,22 +44,22 @@ module.exports = exports = function(schema, options) {
    * @returns Object
    */
 	schema.methods.addReaction = function (reactionType, userId) {
-    if(options.set && !options.set.includes(reactionType)){
-      throw new Error('This reaction is not allowed on this document');
-    }
+		if(options.set && !options.set.includes(reactionType)){
+			return {message: 'This reaction is not allowed on this document'};
+		}
 
 		const newReaction = {
 			reactBy: userId,
 			reaction: reactionType
 		};
 
-    const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString() && x.reaction === reactionType);
-    if(!FOUND){
-      this.reactions = this.reactions.concat([newReaction]);
-      return this.save();
-    }
+		const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString() && x.reaction === reactionType);
+		if(!FOUND){
+			this.reactions = this.reactions.concat([newReaction]);
+			return this.save();
+		}
 
-    return this;
+		return this;
 	};
 
 
@@ -90,16 +90,16 @@ module.exports = exports = function(schema, options) {
    * @returns Object
    */
 	schema.methods.toggleReaction = function (reactionType, userId) {
-    if(options.set && !options.set.includes(reactionType)){
-      throw new Error('This reaction is not allowed on this document');
-    }
+		if(options.set && !options.set.includes(reactionType)){
+			return {message: 'This reaction is not allowed on this document'};
+		}
 
 		for (let i = 0; i < this.reactions.length; i++) {
 			let r = this.reactions[i];
 
 			if (r.reactBy && r.reactBy.toString() === userId.toString() && r.reaction === reactionType) {
 				this.reactions.pull({_id: r._id});
-        return this.save();
+					return this.save();
 			}
 		}
 
@@ -108,13 +108,13 @@ module.exports = exports = function(schema, options) {
 			reaction: reactionType
 		};
 
-    const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString() && x.reaction === reactionType);
-    if(!FOUND){
-      this.reactions = this.reactions.concat([newReaction]);
-      return this.save();
-    }
+		const FOUND = this.reactions.find(x => x.reactBy.toString() === userId.toString() && x.reaction === reactionType);
+		if(!FOUND){
+			this.reactions = this.reactions.concat([newReaction]);
+			return this.save();
+		}
 
-    return this;
+		return this;
 	};
 
 };
